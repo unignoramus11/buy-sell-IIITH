a83ce79b04dcfda208432d6286d9009529585b5d
Mohit Singh <unignoramus@gmail.com>
2025-01-29 21:52:12 +0530

    omg

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,21 +7,21 @@ import { FlipWords } from "@/components/ui/flip-words";
 function Home() {
   React.useEffect(() => {
     setTimeout(() => {
-      window.location.href = "/explore";
+      // window.location.href = "/explore";
     }, 5000);
   }, []);
 
   return (
     <div className="h-screen w-full bg-black flex flex-col items-center justify-center overflow-hidden">
-      <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20">
+      <h1 className="text-[15vw] lg:text-9xl font-bold text-center text-white relative z-20 whitespace-nowrap">
         <FlipWords words={["Buy", "Sell"]} className="text-white" />@ IIITH
       </h1>
-      <div className="w-[40rem] h-40 relative">
+      <div className="w-[40vw] h-10 lg:h-40 relative">
         {/* Gradients */}
-        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
-        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
-        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
-        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
+        <div className="absolute inset-x-4 lg:inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+        <div className="absolute inset-x-4 lg:inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+        <div className="absolute inset-x-10 lg:inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+        <div className="absolute inset-x-10 lg:inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
 
         {/* Core component */}
         <SparklesCore
